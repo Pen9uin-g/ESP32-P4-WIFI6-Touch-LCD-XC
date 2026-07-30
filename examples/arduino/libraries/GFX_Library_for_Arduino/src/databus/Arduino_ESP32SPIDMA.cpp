7683397218dc68ffa84b2def115c6f73dc38f214
Y1hsiaochunnn <y1hsiaochunnn@qq.com>
2026-07-30 12:22:49 +0800

    fix: address latest toolchain build errors

--- examples/arduino/libraries/GFX_Library_for_Arduino/src/databus/Arduino_ESP32SPIDMA.cpp
+++ examples/arduino/libraries/GFX_Library_for_Arduino/src/databus/Arduino_ESP32SPIDMA.cpp
@@ -58,10 +58,12 @@ bool Arduino_ESP32SPIDMA::begin(int32_t speed, int8_t dataMode)
   _speed = (speed == GFX_NOT_DEFINED) ? SPI_DEFAULT_FREQ : speed;
   _dataMode = (dataMode == GFX_NOT_DEFINED) ? SPI_MODE0 : dataMode;
 
+#if !defined(ESP_ARDUINO_VERSION) || (ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3, 3, 6))
   if (!_div)
   {
     _div = spiFrequencyToClockDiv(_speed);
   }
+#endif
 
   // set pin mode
   if (_dc != GFX_NOT_DEFINED)
